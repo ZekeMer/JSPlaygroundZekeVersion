d72a8a2cad444dd8eaa3841aa86d7bfad52b3e12
Zeke Her <herezem@dunwoody.edu>
2025-10-31 11:17:36 -0500

    Solved 2nd problem: Style This Text

--- scripts/script.js
+++ scripts/script.js
@@ -22,7 +22,41 @@ function moveTheThing(){
 }
 
 function styleTheText(){
-    // Your code here
+    const text = document.getElementById("fancyText");
+
+//New Color
+    //Generates a random color hexcode. Code by user "bryc" on stackoverflow.com
+    const newColor = ('00000'+(Math.random()*(1<<24)|0).toString(16)).slice(-6);
+
+    console.log(newColor);
+
+    //Ocothorpe denotes that 'newColor' variable is a color value
+    text.style.color = '#' + newColor;
+
+//New Font
+    const newFont = ['Times New Roman', 'Comic Sans MS', 'Courier New', 'Lucida Handwriting', 'Brush Script MT', 'Papyrus'];
+
+    //newFont.length says that 'newFont' has two values inside it. The Math.random() doesn't ever get last index value.
+    //Example: -- Math.floor(Math.random() * 10); -- Will only ever return values 0-9 (if 9.99 is generated, Math.floor pulls it down to 9)
+    const indexNum = Math.floor(Math.random() * newFont.length);
+
+    console.log(newFont[indexNum]);
+
+    if (indexNum === 5)
+    {
+        console.log("PAPYRUS!!!! NOOO!!!!");
+    }
+    
+    text.style.fontFamily = newFont[indexNum];
+
+//New Size
+    const newSize = Math.floor(Math.random() * 101);
+
+    console.log("The new font size is: " + newSize);
+
+    //Size will not show without added 'px'
+    text.style.fontSize = newSize + 'px';
+
 }
 
 function getFormValues(){
